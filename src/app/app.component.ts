8a9f33dfb1cb668a94a1b581c3a6de13ee3b07ef
Ana Helena <anahelenarp@hotmail.com>
2019-02-18 14:52:29 -0300

    Add service and some jokes with public api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jokes';
+
+  jokes: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.data.gimmeJokes().subscribe(res => {
+      this.jokes = res;
+    })
+  }
 }
